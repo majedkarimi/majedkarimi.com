ebc8c57c6c3bf9eb40041f9b75bd1fa22cd37c93
Majed <majed.karimi3@gmail.com>
2023-09-13 22:29:41 +0330

    fix ui footer

--- feature/footer/Footer.tsx
+++ feature/footer/Footer.tsx
@@ -6,17 +6,19 @@ const Footer = (props: infoType) => {
   return (
     <div className={style.footer}>
       <div className={style.inner}>
-        <div className="copyright">
-          <span>Copyright © 2023 Farsdev</span>
-        </div>
-        <div className={style.information}>
-          <div className={style.mobile}>
-            <span>Phone:</span>
-            <a href={`tel:+98${props.phone}`}>{`0${props.phone}`}</a>
+        <div className="flex flex-col gap-1">
+          <div className="copyright">
+            <span>Copyright © 2023 Farsdev. All rights reserved.</span>
           </div>
-          <div className={style.email}>
-            <span>Email:</span>
-            <a href={`mailto: ${props.email}`}>{props.email}</a>
+          <div className={style.information}>
+            <div className={style.mobile}>
+              <span>Phone:</span>
+              <a href={`tel:+98${props.phone}`}>{`0${props.phone}`}</a>
+            </div>
+            <div className={style.email}>
+              <span>Email:</span>
+              <a href={`mailto: ${props.email}`}>{props.email}</a>
+            </div>
           </div>
         </div>
       </div>
