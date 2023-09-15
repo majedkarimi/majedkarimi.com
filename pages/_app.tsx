8a0eb713bfd44a38d4d2c894ac03906f6ce5cbab
Majed <majed.karimi3@gmail.com>
2023-09-16 00:13:28 +0330

    config google tag manager

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,19 @@ import ProvideSw from "@/config/ProvideSw";
 import Providers from "@/store/Provider";
 import "../style/globals.css";
 import "../style/animation.css";
+import { useEffect } from "react";
+import TagManager, { TagManagerArgs } from "react-gtm-module";
+
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const gtmId = "GTM-PKX6PXF";
+  const tagManagerArgs: TagManagerArgs = {
+    gtmId,
+  };
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs);
+  }, []);
   return (
     <ProvideSw>
       <Providers>
